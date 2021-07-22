c8aebb9b648f09056f19234d178cc3713a7caaec
Henry (Wei) <hyd628@gmail.com>
2021-07-23 07:17:02 +0800

    initial setup

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,26 @@
 import os
+import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
+
+@bot.event
+async def on_message(message):
+
+    # Checking if its a dm channel
+    if isinstance(message.channel, discord.DMChannel):
+        # Getting the channel
+        channel = bot.get_channel(CHANNEL_ID)
+        await channel.send(f"{{{message.author}}} sent:\n```{message.content}```")
+        
+    # Processing the message so commands will work
+    await bot.process_commands(message)
 
 @bot.command()
 async def ping(ctx):
